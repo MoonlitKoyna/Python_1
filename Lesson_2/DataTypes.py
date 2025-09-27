1cd988d14cc9fb12787998172b13cb797a72e5d1
MoonLitKoyna <koynauwu@gmail.com>
2025-09-27 17:48:14 +0530

    initial commit

---
+++ Lesson_2/DataTypes.py
@@ -0,0 +1,20 @@
+name="Koyna"
+age=16
+is_student= True
+weight= 46.5
+
+print("Name : ",name)
+print("Data type of name is, ", type(name))
+print("Age : ", age)
+print("Data type of age is, ", type(age))
+print("is_student ", is_student)
+print("Data type of is_student is, ", type("is_student"))
+print("Weight : ",weight)
+print("Data type od weight is, ", type(weight))
+
+print("\n After type casting.....")
+age=str(age)
+print(age)
+print("Data type of age is, ", type(age))
+weight= int(weight)
+print("Data type of weight is, ", type(weight))
---
+++ Lesson_2/StringOperators.py
@@ -0,0 +1,19 @@
+first_name="Koyna"
+last_name="Chakrabotry"
+full_name="Koyna Chakrabotry"
+example="Haa"*5
+
+print("First Name: ", first_name)
+print("Last Name: ", last_name)
+print("Full name:", full_name)
+print("String multiplied 5 times gives thhis result: ", example)
+
+word="Coding"
+print("Length of string: ", len(word))
+print("First letter of string: ", word[0])
+print("Last letter of string: ", word[5])
+print("String sliced: ", word[0:3])
+
+
+name= input("Enter your name: ")
+print("Your name has ", len(name), "words")
